d2b61bd7b1281bbf6da40e1d0acc1f130eded6cd
Anderson Mesquita <andersonvom@gmail.com>
2012-06-20 14:13:10 -0400

    Opening file and handling error skeleton

--- 01.paradigms/01.assembly/src/cat.asm
+++ 01.paradigms/01.assembly/src/cat.asm
@@ -6,9 +6,24 @@ section .text
 	global _start
 
 _start:
-	pop	ebx
-	dec	ebx
-	pop	ebx
+	pop	ebx		; argc
+	pop	ebx		; argv[0] - program name
+
+open_next_file:
+	pop	ebx		; argv[i] - filename
+	mov	eax,	5	; sys_open
+	mov	ecx,	0	; O_RDONLY
+	int	80h
+
+	test	eax,	eax
+	jns	file_not_found
+	jmp	exit
+	
+read_file:
+	
+
+file_not_found:
+	
 
 exit:
 	mov	eax,	1
